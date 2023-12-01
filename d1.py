c8e71a577bf1fc7cb65c68d9f50295f00af01b43
newt <emailfornc@gmail.com>
2023-12-01 11:14:45 -0500

    clean up a bit

--- d1.py
+++ d1.py
@@ -1,31 +1,10 @@
-# Program to show various ways to
-# read data from a file.
-
-
-
-
-
 def is_digit_in_string(line_string):
-    
-
-    # printing original string 
-    print("The original string : " + line_string)
-
-    # using List comprehension + isdigit() +split()
-    # getting numbers from string 
     res = [int(i) for i in line_string if i.isdigit()]
-
-    # print result
     return res
 
-print('here')
 all = []
 with open("input.txt", "r+") as file1:
-	# Reading from a fil
-    print('here')
-
     for line in file1:
-        print('line')
         values = is_digit_in_string(line)
         if len(values) == 1 :
             all.append(values[0] * 11)
@@ -33,9 +12,17 @@ with open("input.txt", "r+") as file1:
             x = int(str(values[0]) +  str(values[-1]))
             all.append(x)
 
-print('alll')
-print(all)
 cout = 0
 for num in all:
     cout += num
 print(cout)
+
+
+
+echo "# 2023_advent_of_code" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  
+  git push -u origin main
